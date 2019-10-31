860b4fa07bbd67e392c1dfae32098bb195f81753
Tobie <quentin.bogaert@gmail.com>
2019-10-31 11:39:01 +0100

    Ajout d'une page principale javascript avec commentaires de structure

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//évènements :
+
+    //chargement de la page
+
+    //clic sur un marqueur
+
+    //clic sur le bouton d'envoi du formulaire
+
+    //chargement de la carte
+
+    //réponse requête
+
+
+//acions :
+
+    //afficher le slider
+
+    //créer la carte
+
+    //paramétrer la carte
+
+    //créer le modèle de marqueur
+
+    //récupérer les coordonnées gps de chaque station
+
+    //placer un marqueur sur chaque emplacement de station
+
+    //récupérer l'adresse, le nombre de vélos disponibles et le nombre de places libres d'une station
+
+    //afficher l'adresse, le nombre de vélos disponibles et le nombre de places libres d'une station
+
+
+
+
+//créer la carte et l'initialiser
+
+//afficher les marqueurs sur les zones de stations
+
+//ajouter les informations d'une station quand on clique sur le marqueur correspondant
+
+//afficher le formulaire au clic sur un marqueur
+
+//afficher le cadre de signature à l'envoi du formulaire
+
+//à l'envoie d'un formulaire : afficher la réservation avec lieu, nom et temps restant
